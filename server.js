4d71691159197fdc040c1bdf708e0abe10e25a3f
jteng <akariass@gmail.com>
2018-08-08 14:11:25 -0400

    added redis store

--- server.js
+++ server.js
@@ -5,23 +5,27 @@ var express = require('express')
 var app = express()
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin",
-    "http://localhost:4200");
-  res.header("Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
 });
 
-var session = require('express-session')
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
 app.use(session({
+    store: new RedisStore({
+        url: 'redis://h:p7cc735781fad2544be6dbf24401643a06c2d1cea91bc2ebcf9612f8da0317d05@ec2-34-231-81-175.compute-1.amazonaws.com:33259'
+    }),
     resave: false,
     saveUninitialized: true,
     secret: 'secret556',
     cookie: {
-        maxAge: 1000*60*30
+        maxAge: 1000 * 60 * 30
     }
 }));
 
@@ -55,7 +59,7 @@ app.get('/api/session/get/:name', getSession);
 app.get('/api/session/get', getSessionAll);
 app.get('/api/session/reset', resetSession);
 
-require('./services/user.service.server')(app)
-require('./services/section.service.server')(app)
+require('./services/user.service.server')(app);
+require('./services/section.service.server')(app);
 
-app.listen(4000)
+app.listen(process.env.PORT || 4000);
